47439ca0bf50b093da770ff7ea057b044e81e214
ANA KHASANAH <ana.khasanah@capita.com>
2022-10-11 16:28:08 +0100

    Initial commit

---
+++ src/BrokeNotification.java
@@ -0,0 +1,13 @@
+public class BrokeNotification {
+    public static void main(String[] args) {
+        // if statement = performs block of code it its condition evaluates to be tru
+        int balance = 0;
+
+        if (balance < 1) {
+            System.out.println("You are broke!");
+        } else {
+            System.out.println("You have £ " + balance  + " left!");
+        }
+
+    }
+}
